2c53fcafbd1b323d889d6b003742b99292d323eb
ben-denzer <denzer.ben@gmail.com>
2017-07-27 00:23:48 -0700

    memory game functional

--- client/components/MemoryPage.js
+++ client/components/MemoryPage.js
@@ -13,7 +13,9 @@ import {
 
 export default function MemoryPage(props) {
   const {
+    cardList,
     coins,
+    flipCard,
     helpOpen,
     mute,
     score,
@@ -24,11 +26,25 @@ export default function MemoryPage(props) {
     wordList
   } = props;
 
-  console.log(coins);
+  const cards = cardList.map(a => {
+    return (
+      <Card
+        key={a.cardId}
+        className={a.status}
+        data-cardId={a.cardId}
+        onClick={flipCard}
+      >
+        <p>{a.word}</p>
+      </Card>
+    );
+  });
 
   return (
     <div>
       <PageContainer>
+        <CardContainer>
+          {cards}
+        </CardContainer>
         <MuteButton
           src={`/static/img/${mute ? 'mute' : 'unmute'}.png`}
           alt={`Turn Sound ${mute ? 'On' : 'Off'}`}
@@ -37,7 +53,6 @@ export default function MemoryPage(props) {
         <ScoreContainer>
           {coins} <CoinImage src="/static/img/quarter.png" alt="Coins" />
         </ScoreContainer>
-
         <HelpButton
           src="/static/img/help.png"
           alt="Help"
@@ -48,3 +63,39 @@ export default function MemoryPage(props) {
     </div>
   );
 }
+
+const CardContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  padding: 75px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Card = styled.div`
+  height: 200px;
+  width: 125px;
+  margin: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid black;
+  border-radius: 5px;
+
+  &.faceDown {
+    background-color: blue;
+
+    p {
+      display: none;
+    }
+  }
+
+  &.faceUp {
+    background-color: white;
+  }
+
+  &.hidden {
+    opacity: .1;
+    background-color: white;
+  }
+`;
--- client/containers/MemoryPageContainer.js
+++ client/containers/MemoryPageContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import shuffle from '../utils/shuffle';
 import defaultWordList from '../config/defaultSightWords';
 import MemoryPage from '../components/MemoryPage';
 
@@ -6,15 +7,21 @@ export default class MemoryPageContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      cardList: [],
       coins: 0,
       helpOpen: false,
       mute: false,
+      numberOfWords: 4,
       score: 0,
       showPrize: false,
       spinnerClassName: 'hide',
       wordList: []
     };
 
+    this.checkForMatch = this.checkForMatch.bind(this);
+    this.flipCard = this.flipCard.bind(this);
+    this.gameOver = this.gameOver.bind(this);
+    this.setupCards = this.setupCards.bind(this);
     this.toggleHelp = this.toggleHelp.bind(this);
     this.toggleSound = this.toggleSound.bind(this);
   }
@@ -22,6 +29,7 @@ export default class MemoryPageContainer extends Component {
   componentDidMount() {
     const coins = window.localStorage.getItem('coins');
     this.setState({ wordList: defaultWordList });
+    this.setupCards();
   }
 
   addCoin() {
@@ -39,6 +47,69 @@ export default class MemoryPageContainer extends Component {
     );
   }
 
+  checkForMatch(cards) {
+    let finalCards;
+    let gameOver = false;
+    const flippedCards = cards.filter(a => a.status === 'faceUp' && a);
+
+    if (flippedCards[0].word === flippedCards[1].word) {
+      const matchedIds = flippedCards.map(a => a.cardId);
+      finalCards = cards.map(a => {
+        if (matchedIds.indexOf(a.cardId) !== -1) {
+          return Object.assign({}, a, { status: 'hidden' });
+        }
+        return a;
+      });
+
+      // check for winner
+      const hiddenCards = finalCards.filter(a => a.status === 'hidden' && a);
+      if (hiddenCards.length === cards.length) gameOver = true;
+    } else {
+      finalCards = cards.map(a => {
+        if (a.status === 'faceUp') {
+          return Object.assign({}, a, { status: 'faceDown' });
+        }
+        return a;
+      });
+    }
+    this.setState({ cardList: cards });
+    setTimeout(() => {
+      this.setState({ cardList: finalCards });
+      if (gameOver) this.gameOver();
+    }, 3000);
+  }
+
+  flipCard(e) {
+    // early return if there are already 2 cards filpped
+    const cards = this.state.cardList.slice(0);
+    const flippedCards = cards.filter(a => a.status === 'faceUp' && a);
+    if (flippedCards.length > 1) return;
+
+    const cardToFlip = e.target.dataset.cardid;
+    cards[cardToFlip].status = 'faceUp';
+    flippedCards.push(cards[cardToFlip]);
+
+    if (flippedCards.length === 2) {
+      this.checkForMatch(cards);
+    } else {
+      this.setState({ cardList: cards });
+    }
+  }
+
+  gameOver() {
+    alert('You won!');
+  }
+
+  setupCards(numberOfWords = 4, wordList = defaultWordList) {
+    const words = wordList.slice(0, numberOfWords);
+    const cardList = shuffle([...words, ...words]).map((a, i) => ({
+      cardId: i,
+      word: a,
+      status: 'faceDown'
+    }));
+    this.setState({ numberOfWords, cardList });
+  }
+
   toggleHelp() {
     this.setState({ helpOpen: !this.state.helpOpen });
   }
@@ -50,6 +121,7 @@ export default class MemoryPageContainer extends Component {
   render() {
     return (
       <MemoryPage
+        flipCard={this.flipCard}
         toggleHelp={this.toggleHelp}
         toggleSound={this.toggleSound}
         {...this.props}
