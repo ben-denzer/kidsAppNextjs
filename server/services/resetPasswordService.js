6964e0fe9261aa5b68b440ecb5c3f54f4de53214
ben-denzer <denzer.ben@gmail.com>
2017-12-19 00:42:24 -0800

    tests for resetPasswordService

--- server/services/resetPasswordService.js
+++ server/services/resetPasswordService.js
@@ -3,7 +3,7 @@ import { MIN_PASSWORD_LENGTH } from '../../globalConfig/globalConfig';
 import resetPasswordInDb from './resetPasswordInDb';
 import validateJwt from './validateJwt';
 
-function resetPassword(body, connection) {
+function resetPasswordService(body, connection) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (!body || !connection) {
@@ -14,18 +14,20 @@ function resetPassword(body, connection) {
         !body.token
         || !body.password
         || body.password.length < MIN_PASSWORD_LENGTH
-        || !body.p2
         || body.password !== body.p2
       ) {
-        reject({ status: 400, error: 'Bad Request' });
+        return reject({ status: 400, error: 'Bad Request' });
       }
 
       validateJwt(body.token, jwt)
         .then(tokenBody => resetPasswordInDb(tokenBody.email, body.password, connection))
         .then(() => resolve({ success: true }))
-        .catch(e => reject(e));
+        .catch(err => {
+          logError(err, 'in resetPasswordService');
+          reject(err);
+        });
     }, 1500);
   });
 }
 
-export default resetPassword;
+export default resetPasswordService;
--- server/services/resetPasswordService.test.js
+++ server/services/resetPasswordService.test.js
@@ -1,10 +1,76 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import jwt from 'jsonwebtoken';
-import { jwtKey } from '../keys/.jwtInfo';
+import createUserToken from './createUserToken';
 import resetPasswordService from './resetPasswordService';
 const expect = chai.expect;
 
 describe('Reset Password Service', function() {
+  let body;
+  let connection;
+  let token;
+
+  beforeEach('mocking', async function() {
+    global.logError = sinon.stub();
+    connection = { query() {} };
+    token = await createUserToken(12, jwt);
+    body = {
+      token,
+      password: 'fakePassword',
+      p2: 'fakePassword'
+    };
+  });
+
+  it('should reject 500 for null arguments', function() {
+    return resetPasswordService(body)
+      .then(success => expect(success).to.be.false)
+      .catch(err => {
+        expect(logError.calledOnce).to.be.true;
+        expect(err.status).to.equal(500);
+      });
+  });
+
+  it('should reject 400 if no token', function() {
+    body = Object.assign({}, body, { token: undefined });
+    return resetPasswordService(body, connection)
+      .then(success => expect(success).to.be.false)
+      .catch(err => {
+        expect(logError.called).to.be.false;
+        expect(err.status).to.equal(400);
+      });
+  });
+
+  it('should reject 400 if no password', function() {
+    body = Object.assign({}, body, { password: undefined, p2: undefined });
+    return resetPasswordService(body, connection)
+      .then(success => expect(success).to.be.false)
+      .catch(err => {
+        expect(logError.called).to.be.false;
+        expect(err.status).to.equal(400);
+      });
+  });
+
+  it('should reject 400 if password is too short', function() {
+    body = Object.assign({}, body, { password: 'abc', p2: 'abc' });
+    return resetPasswordService(body, connection)
+      .then(success => expect(success).to.be.false)
+      .catch(err => {
+        expect(logError.called).to.be.false;
+        expect(err.status).to.equal(400);
+      });
+  });
+
+  it('should reject 400 if passwords dont match', function() {
+    body = Object.assign({}, body, { password: 'asdofoeoire' });
+    return resetPasswordService(body, connection)
+      .then(success => expect(success).to.be.false)
+      .catch(err => {
+        expect(logError.called).to.be.false;
+        expect(err.status).to.equal(400);
+      });
+  });
+
+  it('should reject on validateJwt error');
+  it('should reject on resetPasswordInDb error');
 
 });
